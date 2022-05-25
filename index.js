0b1b1fa41ae15d0e47844638549866913a5b74d8
ANAIS <anais.bustamante@unmsm.edu.pe>
2022-05-25 01:25:39 -0500

    Titulo del commit
    Descripcion del commit
    -Item 1
    -Item 2
    -Item 3

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ const getPosts = async (user) => {
 
 const getCommentsForEachPost = async (posts) => {
   const res = await Promise.all(posts.map(post => 
-<<<<<<< HEAD
-    fetch(`${url}/comments?postId=${post.id}&_limit=3`)  
-=======
-    fetch(`${url}/comments?postId=${post.id}&_limit=4`)  
->>>>>>> b34698455d4fee995eceec929a2925ab81670a74
+    fetch(`${url}/comments?postId=${post.id}&_limit=5`)  
   ))
   const postComments = await Promise.all(res.map(r => r.json()));
   
@@ -59,3 +55,5 @@ getBlogContent();
 const loadAdds =() =>{
   console.log('Adds loaded');
 }
+
+//test commit
